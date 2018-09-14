d2c20c41e69e123ac63e2f909b007b0ef58808c9
nicolajfirst <nicolajfirst@gmail.com>
2018-09-14 16:49:15 +0300

    adaptive site

---
+++ 13_test-welcome/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.js-burger').click(function() {
+        $('.js-list').slideToggle(500);
+    });
+
+    $(window).resize(function () {
+        if ($(window).width() > 780) {
+            $('.js-list').removeAttr('style');
+        }
+    });
+});
